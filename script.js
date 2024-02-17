17b8705fc67bc1160e49dc1a36fff3cd6e5741c4
jamessouth65 <158448383+jamessouth65@users.noreply.github.com>
2024-02-17 00:30:28 -0500

    test1

---
+++ script.js
@@ -0,0 +1,70 @@
+function getPasswordLength() {
+    const length = document.getElementById('length').value;
+    return Number(length);
+}
+
+function getPasswordProperties() {
+    const ids = ["lowercase", "uppercase", "numbers", "special"];
+    const properties = {};
+
+    for (const id of ids) {
+        const element = document.getElementById(id);
+        properties[id] = element.checked;
+}
+
+return properties;
+}
+
+function getChars(Lowercase = true) { 
+    const start = Lowercase ? 97 : 65;
+    let chars = [];
+
+
+    for (let i = start; i < start + 26; i++) {
+        chars.push(String.fromCharCode(i));
+    }
+
+    return chars;
+}
+
+function getNumbers() {
+    const nums = [];
+
+    for (let i = 0; i < 10; i++) {
+        nums.push(i);
+}
+
+    return nums;
+}
+   
+    const lowercaseChars = getChars(true);
+    const uppercaseChars = getChars(false);
+    const numbers = getNumbers();
+    const special = ["!", "@", "#", "%", "^", "&", "*", "(", ")", "-", "_"];
+
+function generatePassword() {
+    const length = getPasswordLength();
+    const properties = getPasswordProperties();
+
+    const characters = [];
+    if (properties.lowercase) characters.push(...lowercaseChars);
+    if (properties.uppercase) characters.push(...uppercaseChars);
+    if (properties.numbers) characters.push(...numbers);
+    if (properties.special) characters.push(...special);
+    
+    if (characters.length === 0) {
+       return alert("You must select at least one set of characters.");
+
+    }
+    
+    let pwd = [];
+    for (let i = 0; i < length; i++) {
+        const randomIdx = Math.floor(Math.random() * characters.length);
+        const char = characters[randomIdx];
+        pwd.push(char);
+    }
+        const pwdstring = pwd.join("");
+        document.getElementById("password").innerHTML = "<p>" + pwdstring + "</p>";
+
+
+    }
